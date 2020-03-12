300ad8e5afeab28416fc73552eb071a95784e250
Benjamin Monteiro Da Silva <benjamin.monteirodasilva@gmail.com>
2020-03-12 16:15:49 +0100

    Suppression des use inutile des controllers

--- src/Controller/Admin/UtilityController.php
+++ src/Controller/Admin/UtilityController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Admin;
 
 use App\Controller\Admin\AdminController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CompanyRepository;
--- src/Form/PerformanceType.php
+++ src/Form/PerformanceType.php
@@ -3,10 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Performance;
-use App\Entity\Event;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\Form\DataTransformer\CompanyTransformer;
